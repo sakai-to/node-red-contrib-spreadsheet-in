e112d58f8d9620e9ff8eeb846d92fa42a10e90cf
SAKAI Toshiharu <sakai.to@gmail.com>
2017-10-17 00:22:36 +0900

    add raw, header and blankrows parameters

--- sheet-to-json.js
+++ sheet-to-json.js
@@ -1,17 +1,25 @@
 module.exports = function(RED) {
 	const XLSX = require('xlsx');
 
+	function convertHeaderParam(value) {
+		switch (value) {
+			case "1": return 1;
+			case "A": return "A";
+			default: return undefined;
+		}
+	}
+
 	function SheetToJsonNode(config) {
 		RED.nodes.createNode(this,config);
 		var node = this;
 		node.on('input', function(msg) {
 			var option = {
-				raw: false,
+				raw: config.raw == "true",
 				range: config.range || "!ref",
-				header: undefined,
+				header: convertHeaderParam(config.header),
 				dateNF: undefined,
 				defval: undefined,
-				blankfows: undefined
+				blankrows: config.blankrows == "true"
 			};
 			msg.payload = XLSX.utils.sheet_to_json(msg.payload, option);
 			node.send(msg);
